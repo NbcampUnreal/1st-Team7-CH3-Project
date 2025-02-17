e7729c7f7f9c40b123d2dc7b7993c46bb78d26c5
erldfd <hseongrae@gmail.com>
2025-02-17 15:13:27 +0900

    기본 설정
    테스트맵 생성
    캐릭터 베이스 생성
    게임 인스턴스 생성
    게임 인스턴스 등록
    기본 맵 등록

--- Source/Cyberpunk/Cyberpunk.Build.cs
+++ Source/Cyberpunk/Cyberpunk.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class Cyberpunk : ModuleRules
@@ -12,12 +13,17 @@ public class Cyberpunk : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicIncludePaths.AddRange(new string[] 
+        {
+            "Cyberpunk"
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
