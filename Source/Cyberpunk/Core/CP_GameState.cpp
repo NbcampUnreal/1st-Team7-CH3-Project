984e81b558578d4690ff8d6e0801fa9f24d9e101
Big_Baek <euihyun1188@gmail.com>
2025-02-19 17:55:10 +0900

    기본틀 작성

--- Source/Cyberpunk/Core/CP_GameState.cpp
+++ Source/Cyberpunk/Core/CP_GameState.cpp
@@ -1,16 +1,74 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "Core/CP_GameState.h"
+#include "Character/CP_PlayerController.h"
 
 ACP_GameState::ACP_GameState()
 {
-
+	Wave = 0;
+	WatingTime = 3.0f;
+	AI_Count = 0;
 }
 
 void ACP_GameState::BeginPlay()
 {
 	Super::BeginPlay();
 
+	StartWave();
 
+}
+
+void ACP_GameState::StartWave()
+{
+	SpawnAI();
+	//AI스폰이후 WatingTime초가 지나면 이동시작 이동함수 미구현으로 주석처리.
+	/*GetWorldTimerManager().SetTimer(
+		AIWatingTimerHandel,
+		this,
+		&CP_EnemyAI::StartMove,
+		WatingTime,
+		false
+	);*/
+
+
+
+
+}
+
+void ACP_GameState::StartBossWave()
+{
+}
+
+void ACP_GameState::OnGameOver()
+{
+	if (APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
+	{
+		if (ACP_PlayerController* CP_PlayerController = Cast<ACP_PlayerController>(PlayerController))
+		{
+			CP_PlayerController->SetPause(true);
+			// CP_PlayerController->ShowMainMenu(true); //미구현
+		}
+	}
+}
+
+void ACP_GameState::KillAll()
+{
+	if (AI_Count >= 0 && Wave <= 3)// ai가 모두 죽고 현재 웨이브가 2이하면 다음 웨이브 진행
+	{
+		StartWave();
+	}
+	else if (AI_Count >= 0 && Wave > 2) // 3웨이브를 끝냈다면 보스전 진행
+	{
+		StartBossWave();
+	}
+	else //보스전 종료
+	{
+		if (Wave > 3)
+		{
+			OnGameOver();
+		}
+	}
+}
+
+void ACP_GameState::SpawnAI()
+{
 }
--- Source/Cyberpunk/Core/CP_GameState.h
+++ Source/Cyberpunk/Core/CP_GameState.h
@@ -14,7 +14,19 @@ public:
 	ACP_GameState();
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Level")
-	int32 WaveCount;
 
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Level")
+	int32 Wave;
+	int32 AI_Count;
+	float WatingTime;
+
+
+	FTimerHandle AIWatingTimerHandel;
+
+	void StartWave(); //Wave 시작 함수
+	void StartBossWave(); //보스전 시작 함수
+	void OnGameOver(); //캐릭터 체력이 0이 됬을 경우 호출
+	void KillAll(); //모든 적을 잡았을 때 호출
+	void SpawnAI(); //웨이브가 새로 시작할 때 적을 생성하는 함수
 };
