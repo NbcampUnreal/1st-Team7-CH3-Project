7767f277c05442048dc08b6484b431626a40de01
erldfd <hseongrae@gmail.com>
2025-02-20 20:56:52 +0900

    Turret, TurretEnemyAI,
    # Turret
    - 일단 기본으로 회전하는 AI만 현재 가능
    - 그 외 AI 작업 중

---
+++ Source/Cyberpunk/AI/CP_BTTRotateTurret.cpp
@@ -0,0 +1,56 @@
+#include "AI/CP_BTTRotateTurret.h"
+
+#include "Cyberpunk.h"
+
+#include "Character/CP_Turret.h"
+#include "AIController.h"
+
+UCP_BTTRotateTurret::UCP_BTTRotateTurret()
+{
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UCP_BTTRotateTurret::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type Result = Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	ACP_Turret* Turret = Cast<ACP_Turret>(OwnerComp.GetAIOwner()->GetPawn());
+	if (Turret == nullptr)
+	{
+		CP_LOG(Warning, TEXT("Turret == nullptr, Name : %s "), *OwnerComp.GetAIOwner()->GetPawn()->GetName());
+		return EBTNodeResult::Failed;
+	}
+	
+	Turret->SetShouldRotate(true);
+
+	return EBTNodeResult::InProgress;
+}
+
+void UCP_BTTRotateTurret::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+
+	ACP_Turret* Turret = Cast<ACP_Turret>(OwnerComp.GetAIOwner()->GetPawn());
+	if (Turret == nullptr)
+	{
+		CP_LOG(Warning, TEXT("Turret == nullptr, Name : %s "), *OwnerComp.GetAIOwner()->GetPawn()->GetName());
+		FinishLatentAbort(OwnerComp);
+		return;
+	}
+
+	if (Turret->GetShouldRotate() == false)
+	{
+		CP_LOG(Log, TEXT("Rotate Finished, Name : %s "), *OwnerComp.GetAIOwner()->GetPawn()->GetName());
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return;
+	}
+
+	if (Turret->IsDead())
+	{
+		CP_LOG(Log, TEXT("Dead.. Rotate Finished, Name : %s "), *OwnerComp.GetAIOwner()->GetPawn()->GetName());
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return;
+	}
+
+	Turret->RotateTurret(Turret->GetRotateSpeed() * DeltaSeconds);
+}
---
+++ Source/Cyberpunk/Character/CP_Turret.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CP_Enemy.h"
+
+#include "CP_Turret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERPUNK_API ACP_Turret : public ACP_Enemy
+{
+	GENERATED_BODY()
+
+public:
+
+	ACP_Turret();
+
+public:
+
+	virtual void BeginPlay() override;
+	
+	virtual void AttackNormal() override;
+
+	// Yaw 회전
+	void RotateTurret(float DeltaRotation);
+	// 플레이어 바라보기
+	void AimPlayer(const class ACP_Player* Player);
+
+public:
+
+	float GetRotateSpeed();
+
+	bool GetShouldRotate();
+	void SetShouldRotate(bool bNewShouldRotate);
+
+protected:
+
+	void RotatePitch(float DeltaRotation);
+	void RotateBody(float DeltaRotation);
+	void RotateLowerBody(float DeltaRotation);
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Turret")
+	float RotateSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Turret")
+	TSet<FName> UpperBodyBones;
+
+	UPROPERTY(EditAnywhere, Category = "Turret")
+	TSet<FName> LowerBodyBones;
+
+	UPROPERTY(VisibleAnywhere, Category = "Turret")
+	TObjectPtr<USkeletalMeshComponent> LowerBodyMeshComp;
+
+	float OriginalMeshRelativeYaw = 0;
+
+	bool bShouldRotate = false;
+
+};
+
