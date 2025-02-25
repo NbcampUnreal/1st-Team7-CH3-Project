cb1cd913bba3d23a011ff0279c796fe279b3b023
erldfd <hseongrae@gmail.com>
2025-02-25 17:45:26 +0900

    엄폐 Enemy AI 로직 추가
    엄폐를 하며 도망치다 플레이어팀의 유닛이 사거리에 들어오면 공격 시작

---
+++ Source/Cyberpunk/AI/Decorator/CP_BTDIsPlayerInAttackRange.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "CP_BTDIsPlayerInAttackRange.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERPUNK_API UCP_BTDIsPlayerInAttackRange : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+};
