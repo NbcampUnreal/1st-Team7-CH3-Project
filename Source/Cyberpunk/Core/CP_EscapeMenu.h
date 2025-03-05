1a8ffeac0a50886382cf83ad6f219dd108e4cbb5
Pelnatt <hunlove65@gmail.com>
2025-03-05 15:55:36 +0900

    완성 - 메인메뉴, 일시정지, 디자인 - 사망메뉴, 승리메뉴

---
+++ Source/Cyberpunk/Core/CP_EscapeMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CP_EscapeMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERPUNK_API UCP_EscapeMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
